8bef8525c432a79e31579af32bba2d893958ac36
BrotskyS <sergiypik.work@gmail.com>
2022-02-15 22:11:31 +0200

    Initial Commit

---
+++ AllCalculators/AllCalculatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllCalculatorsApp.swift
+//  AllCalculators
+//
+//  Created by Sergiy Pik on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AllCalculatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
